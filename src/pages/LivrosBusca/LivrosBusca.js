f50cfc54645b8f300ba1a00b97c655edbd9d172a
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-08-29 15:05:38 -0300

    [feature] tela de busca de livros

---
+++ src/pages/LivrosBusca/LivrosBusca.js
@@ -0,0 +1,28 @@
+import React, { useState }  from "react";
+
+export default function LivrosBusca(){
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (e) => {
+      setSearchTerm(e.target.value);
+    };
+
+    return(
+        <main>
+            <div className="min-h-screen bg-gray-100 p-8">
+                <div className="container mx-auto max-w-4xl bg-white p-6 rounded-lg shadow-md">
+                    <h1 className="text-3xl font-bold mb-4">Buscar Livros</h1>
+                    <div className="mb-6">
+                        <input
+                            type="text"
+                            placeholder="Buscar livros..."
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300"
+                        />
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
