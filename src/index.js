803d65557fce526c3b538c49873d74a2f529946f
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-08-29 21:22:35 -0300

    [feature] navegação e pagina inicial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import Header from './components/Header/Header';
+import Inicial from './pages/Inicial/Inicial';
+import LivrosBusca from "./pages/LivrosBusca/LivrosBusca";
+import LivrosCadastro from "./pages/LivrosCadastro/LivrosCadastro";
+import LivrosShow from "./pages/LivrosShow/LivrosShow";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Inicial />,
+  },
+  {
+    path: "/livro",
+    element: <LivrosShow />,
+  },
+  {
+    path: "/livro/busca",
+    element: <LivrosBusca />,
+  },
+  {
+    path: "/livro/cadastro",
+    element: <LivrosCadastro />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/Inicial/Inicial.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Inicial(){
+
+    return(
+        <main>
+            <h1>PAGINA INICIAL</h1>
+        </main>
+    )
+}
