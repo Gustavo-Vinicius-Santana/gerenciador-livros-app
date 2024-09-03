f06e07634dd5d0bda554184998e9297870db4425
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-09-02 23:28:09 -0300

    [feature] modal de detalhes ao clicar no card do livro

--- src/pages/Livros/LivrosShow/LivrosShow.js
+++ src/pages/Livros/LivrosShow/LivrosShow.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { Modal } from "flowbite-react";
 import { getLivros } from '../../../services/LivroService';
 
 export default function LivrosShow(){
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [selectedBook, setSelectedBook] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = (book) => {
+        setSelectedBook(book);
+        setIsOpen(true);
+      };
+
+    const closeModal = () => {
+        setIsOpen(false);
+        setSelectedBook(null);
+    };
 
     useEffect(() => {
         const fetchLivros = async () => {
@@ -20,6 +33,8 @@ export default function LivrosShow(){
       fetchLivros();
     }, []);
 
+
+
     if (loading) return <p>Carregando...</p>;
 
     return(
@@ -29,17 +44,55 @@ export default function LivrosShow(){
                     <h1 className="text-3xl font-bold mb-8 text-center">Lista de Livros</h1>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {books.map((book) => (
-                        <div key={book.id} className="bg-white p-6 rounded-lg shadow-lg">
-                        <h2 className="text-xl font-semibold mb-2">{book.titulo}</h2>
-                        <p className="text-gray-700 mb-4">{book.resumo}</p>
-                        <p className="text-gray-700 mb-4">{book.ano_lancamento}</p>
-                        <p className="text-gray-600">Autor: {book.autores_id}</p>
-                        <p className="text-gray-600">Editora: {book.editoras_id}</p>
+                        <div onClick={() => openModal(book)} key={book.id} className="cursor-pointer bg-white p-6 rounded-lg shadow-lg">
+                            <h2 className="text-xl font-semibold mb-2">{book.titulo}</h2>
+                            <p className="text-gray-700 mb-4">{book.resumo}</p>
+                            <p className="text-gray-700 mb-4">{book.ano_lancamento}</p>
+                            <p className="text-gray-600">Autor: {book.autores_id}</p>
+                            <p className="text-gray-600">Editora: {book.editoras_id}</p>
                         </div>
                     ))}
                     </div>
                 </div>
             </div>
+
+
+            {isOpen && selectedBook && (
+                <div className={`fixed inset-0 bg-black bg-opacity-50 z-40 flex items-center justify-center`}>
+                    <Modal
+                        className="z-50"
+                        size={`md`}
+                        show={isOpen}
+                        onClose={closeModal}
+                    >
+                        <Modal.Header className="p-5">{selectedBook.titulo}</Modal.Header>
+                            <Modal.Body>
+                                <p><strong>Resumo:</strong> {selectedBook.resumo}</p>
+                                <p><strong>Ano de Lançamento:</strong> {selectedBook.ano_lancamento}</p>
+                                <p><strong>Autor:</strong> {selectedBook.autores_id}</p>
+                                <p><strong>Editora:</strong> {selectedBook.editoras_id}</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <div className="w-full flex justify-between p-4">
+                                    <button
+                                        className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                        onClick={() => alert('Deletar')}
+                                    >
+                                        Deletar
+                                    </button>
+
+                                    <button
+                                        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                        onClick={() => alert('Editar')}
+                                    >
+                                        Editar
+                                    </button>
+                                </div>
+                            </Modal.Footer>
+                    </Modal>
+                </div>
+            )}
+
         </main>
     )
 }
