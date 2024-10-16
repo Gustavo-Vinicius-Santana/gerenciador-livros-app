0317bf08166baf1fe13936f057e9feba065ae3ba
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-10-16 17:39:18 -0300

    [feature] criando diretorio das rotas

--- src/index.js
+++ src/index.js
@@ -1,121 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import Header from './components/Header/Header';
-import Inicial from './pages/Inicial/Inicial';
-
-// paginas dos livros
-import LivrosBusca from "./pages/Livros/LivrosBusca/LivrosBusca";
-import LivrosCadastro from "./pages/Livros/LivrosCadastro/LivrosCadastro";
-import LivrosShow from "./pages/Livros/LivrosShow/LivrosShow";
-import LivroEdit from './pages/Livros/LivroEdit/LivroEdit';
-
-// paginas editoras
-import EditoraBusca from './pages/Editoras/EditorasBusca/EditoraBusca';
-import EditoraCadastro from './pages/Editoras/EditorasCadastro/EditoraCadastro';
-import EditoraShow from './pages/Editoras/EditorasShow/EditoraShow';
-import EditoraEdit from './pages/Editoras/EditoraEdit/EditoraEdit';
-
-// paginas autores
-import AutorBusca from './pages/Autores/AutoresBusca/AutorBusca';
-import AutorCadastro from './pages/Autores/AutoresCadastro/AutorCadastro';
-import AutoresShow from './pages/Autores/AutoresShow/AutoresShow';
-import AutorEdit from './pages/Autores/AutorEdit/AutorEdit';
-
-// paginas usuarios
-import UsuarioLogin from './pages/Usuario/UsuarioLogin/UsuarioLogin';
-import UsuarioCadastro from './pages/Usuario/UsuarioCadastro/UsuarioCadastro';
-import UsuarioTela from './pages/Usuario/UsuarioTela/UsuarioTela';
-import UsuarioEdit from './pages/Usuario/UsuarioEdit/UsuarioEdit';
-
-import { AuthProvider } from './contexts/AuthProvider';
+import ReactDOM from 'react-dom/client';
 
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Inicial />,
-  },
-  {
-    path: "/livro",
-    element: <LivrosShow />,
-  },
-  {
-    path: "/livro/busca",
-    element: <LivrosBusca />,
-  },
-  {
-    path: "/livro/cadastro",
-    element: <LivrosCadastro />,
-  },
-  {
-    path: "/livro/editar/:id",
-    element: <LivroEdit />,
-  },
+import routerPages from './routes/routesPage';
 
-  {
-    path: "/editora",
-    element: <EditoraShow />,
-  },
-  {
-    path: "/editora/busca",
-    element: <EditoraBusca />,
-  },
-  {
-    path: "/editora/cadastro",
-    element: <EditoraCadastro />,
-  },
-  {
-    path: "/editora/editar/:id",
-    element: <EditoraEdit />,
-  },
+import Header from './components/Header/Header';
+import { AuthProvider } from './contexts/AuthProvider';
 
-  {
-    path: "/autor",
-    element: <AutoresShow />,
-  },
-  {
-    path: "/autor/busca",
-    element: <AutorBusca />,
-  },
-  {
-    path: "/autor/cadastro",
-    element: <AutorCadastro />,
-  },
-  {
-    path: "/autor/editar/:id",
-    element: <AutorEdit />,
-  },
-
-  {
-    path: "/usuario/login",
-    element: <UsuarioLogin />
-  },
-  {
-    path: "/usuario/cadastro",
-    element: <UsuarioCadastro />
-  },
-  {
-    path: "/usuario/tela",
-    element: <UsuarioTela />
-  },
-  {
-    path: "/usuario/editar",
-    element: <UsuarioEdit />
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <Header />
-      <RouterProvider router={router} />
+      <RouterProvider router={routerPages} />
     </AuthProvider>
   </React.StrictMode>
 );
