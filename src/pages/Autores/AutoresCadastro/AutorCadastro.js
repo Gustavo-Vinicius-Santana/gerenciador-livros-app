548d20644e297c593599f1b4502d9a742e370aef
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-10-22 11:51:25 -0300

    [fix] adicionando componentes nas paginas de cadastro e edit

--- src/pages/Autores/AutoresCadastro/AutorCadastro.js
+++ src/pages/Autores/AutoresCadastro/AutorCadastro.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
 import { useAutorData } from "../../../services/hooks/useAutorData";
+
+import InputText from "../../../components/Forms/Inputs/InputText";
+import Botao from "../../../components/Forms/Buttons/Button";
 import ToastAviso from "../../../components/Toasts/ToastAviso";
 import LoadingOverlay from "../../../components/Loadings/LoadingOverlay";
 
@@ -37,30 +40,21 @@ export default function AutorCadastro(){
             <div className="min-h-screen bg-gray-100 p-8">
                 <ToastAviso show={showToast} setShow={setShowToast} mensagem={mensagem} />
                 <div className="container mx-auto max-w-4xl bg-white p-6 rounded-lg shadow-md">
-                    <h1 className="text-3xl font-bold mb-6">Cadastro de Autor</h1>
+                    <h1 className="text-3xl font-bold mb-6">
+                        Cadastro de Autor
+                    </h1>
+
                     <form onSubmit={handleSubmit} className="space-y-6">
-                    <div>
-                        <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
-                        Nome
-                        </label>
-                        <input
-                        id="name"
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Digite o nome do autor"
-                        className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300"
-                        required
-                        />
-                    </div>
-                    <div>
-                        <button
-                        type="submit"
-                        className="w-full py-3 bg-blue-500 text-white font-medium rounded-lg hover:bg-blue-600 transition duration-300"
-                        >
-                        Cadastrar Autor
-                        </button>
-                    </div>
+                        <div>
+                            <InputText id="name" titulo="Nome"
+                                placeholder="Digite o nome do autor"
+                                tipo="text" valor={name} setValor={setName}
+                            />
+                        </div>
+
+                        <div>
+                            <Botao titulo="cadastrar autor" />
+                        </div>
                     </form>
                 </div>
             </div>
